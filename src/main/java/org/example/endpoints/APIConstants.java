6a42c8c9b2659c41b7b6c8976836c28917d3a92c
HP <saurabhtyagi020@gmail.com>
2025-04-03 14:46:18 +0530

    3 April

---
+++ src/main/java/org/example/endpoints/APIConstants.java
@@ -0,0 +1,11 @@
+package org.example.endpoints;
+
+public class APIConstants {
+
+    public static String BASE_URL="https://restful-booker.herokuapp.com";
+    public static String Create_update_Booking="/booking";
+    public static String PING_URL="/ping";
+    public static String AUTH_URL="/auth";
+
+
+}
