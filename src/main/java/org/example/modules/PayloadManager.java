833428ee00db865b5a5676e0ce9ae29cef975678
HP <saurabhtyagi020@gmail.com>
2025-04-03 18:14:49 +0530

    3 April

--- src/main/java/org/example/modules/PayloadManager.java
+++ src/main/java/org/example/modules/PayloadManager.java
@@ -34,8 +34,32 @@ public class PayloadManager {
 
     }
 
-//    public String fullupdatebooking()
-//    {
-//        Booking booking = new Booking();
-//    }
+    public String fullupdatebooking()
+    {
+        Booking booking = new Booking();
+        booking.setFirstname("SHREE");
+        booking.setLastname("RAM");
+        booking.setDepositpaid(true);
+        booking.setTotalprice(1900);
+
+        Bookingdates bookingdates = new Bookingdates();
+        bookingdates.setCheckin("2022-02-02");
+        bookingdates.setCheckout("2024-02-02");
+        booking.setAdditionalneeds("Lunch");
+        booking.setBookingdates(bookingdates);
+
+        Gson gson = new Gson();
+        String updatejsonstring=gson.toJson(booking);
+        return updatejsonstring;
+
+    }
+
+    public Booking getbookingresponse(String response)
+    {
+        Gson gson = new Gson();
+        Booking updatestringresponse=gson.fromJson(response,Booking.class);
+        return updatestringresponse;
+
+    }
+
 }
--- src/test/java/org/example/tests/crud/UpdateBooking.java
+++ src/test/java/org/example/tests/crud/UpdateBooking.java
@@ -2,18 +2,38 @@ package org.example.tests.crud;
 
 import org.example.base.BaseTest;
 import org.example.endpoints.APIConstants;
+import org.example.pojos.Booking;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class UpdateBooking extends BaseTest {
 
-    String bookingid="";
-    String token = "";
+    String bookingid="1333";
+    String token = "5fd7ea5b6f94d68";
+
+    @Test(priority = 2)
     public void test_update_booking()
     {
         requestSpecification.basePath(APIConstants.Create_update_Booking+"/"+bookingid);
-        requestSpecification.body(payloadManager.createbookingpayload());
+        requestSpecification.body(payloadManager.fullupdatebooking());
         requestSpecification.cookie("token",token);
 
         response = requestSpecification.when().put();
+
+        validatableResponse = response.then().statusCode(200);
+        validatableResponse.log().all();
+
+        Assert.assertEquals(200,response.getStatusCode());
+        Booking responseasString = payloadManager.getbookingresponse(response.asString());
+        String fName=responseasString.getFirstname();
+        Assert.assertEquals(fName,"SHREE");
+
+        assertThat(responseasString.getFirstname()).isNotBlank().isEqualTo("SHREE").isNotNull();
+        String checkindate= responseasString.getBookingdates().getCheckin();
+        System.out.println(checkindate);
+        assertThat(responseasString.getBookingdates().getCheckin()).isNotNull();
     }
 
 }
